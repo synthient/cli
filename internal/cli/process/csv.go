6547f011740bc23cadd642add7e76952ae775051
Matt Gleich <git@mattglei.ch>
2025-10-30 15:09:40 -0400

    feat: allow user to select which columns they want to add
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- internal/cli/process/csv.go
+++ internal/cli/process/csv.go
@@ -30,38 +30,52 @@ func GetIpColumn(cr *csv.Reader) int {
 	return column
 }
 
-type LookupColumnSelection int
-
 const (
-	IpColumn LookupColumnSelection = iota
-	NetworkAsnColumn
-	NetworkIspColumn
-	NetworkTypeColumn
-	LocationCityColumn
-	LocationStateColumn
-	LocationCountryColumn
-	LocationTimezoneColumn
-	LocationLongitudeColumn
-	LocationLatitudeColumn
-	LocationGeoHashColumn
-	IPDataDeviceCountColumn
-	IPDataBehaviorColumn
-	IPDataCategoriesColumn
-	IPDataIPRiskColumn
-	IPDataEnrichedColumn
+	IpColumn                = "ip"
+	NetworkAsnColumn        = "network.asn"
+	NetworkIspColumn        = "network.isp"
+	NetworkTypeColumn       = "network.type"
+	LocationCityColumn      = "location.city"
+	LocationStateColumn     = "location.state"
+	LocationCountryColumn   = "location.country"
+	LocationTimezoneColumn  = "location.timezone"
+	LocationLongitudeColumn = "location.longitude"
+	LocationLatitudeColumn  = "location.latitude"
+	LocationGeoHashColumn   = "location.geohash"
+	IPDataDeviceCountColumn = "ipdata.devicecount"
+	IPDataBehaviorColumn    = "ipdata.behavior"
+	IPDataCategoriesColumn  = "ipdata.categories"
+	IPDataIPRiskColumn      = "ipdata.iprisk"
+	IPDataEnrichedColumn    = "ipdata.enriched"
 )
 
-func SelectColumnsToAdd() []LookupColumnSelection {
-	var columns []LookupColumnSelection
-	options := []huh.Option[LookupColumnSelection]{
+func SelectColumnsToAdd() []string {
+	var columns []string
+	options := []huh.Option[string]{
 		huh.NewOption("IP", IpColumn),
-		huh.NewOption("Network Asn", NetworkAsnColumn),
-		huh.NewOption("Network ISP", NetworkIspColumn),
+
+		huh.NewOption("Network: Asn", NetworkAsnColumn),
+		huh.NewOption("Network: ISP", NetworkIspColumn),
+		huh.NewOption("Network: Type", NetworkTypeColumn),
+
+		huh.NewOption("Location: City", LocationCityColumn),
+		huh.NewOption("Location: State", LocationStateColumn),
+		huh.NewOption("Location: Country", LocationCountryColumn),
+		huh.NewOption("Location: Timezone", LocationTimezoneColumn),
+		huh.NewOption("Location: Longitude", LocationLongitudeColumn),
+		huh.NewOption("Location: Latitude", LocationLatitudeColumn),
+		huh.NewOption("Location: Geo Hash", LocationGeoHashColumn),
+
+		huh.NewOption("IP Data: Device Count", IPDataDeviceCountColumn),
+		huh.NewOption("IP Data: Behavior", IPDataBehaviorColumn),
+		huh.NewOption("IP Data: Categories", IPDataCategoriesColumn),
+		huh.NewOption("IP Data: IP Risk", IPDataIPRiskColumn),
+		huh.NewOption("IP Data: Enriched", IPDataEnrichedColumn),
 	}
 	for i, opt := range options {
 		options[i] = opt.Selected(true)
 	}
-	err := huh.NewMultiSelect[LookupColumnSelection]().Title("Data you want to add").
+	err := huh.NewMultiSelect[string]().Title("Data you want to add").
 		Description("These columns will get appended to your csv file.").
 		Options(options...).
 		WithTheme(output.HuhTheme).
