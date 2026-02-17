3e38ee6acfa512364b0031d619f2feddf4a272f7
Matt Gleich <git@mattglei.ch>
2026-02-16 23:55:06 -0500

    feat: bump version

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -7,7 +7,7 @@ import (
 
 var Root = &cobra.Command{
 	Use:     "synthient",
-	Version: "v1.1.0",
+	Version: "v1.2.0",
 	Short:   "Official CLI tool for Synthient [https://github.com/synthient/cli]",
 	Run: func(cmd *cobra.Command, args []string) {
 		err := cmd.Help()
