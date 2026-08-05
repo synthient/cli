9273d129cffa1ec6fafba409b1df2abbfea11864
Matt Gleich <git@mattglei.ch>
2026-08-04 22:01:50 -0400

    chore: bump version to v1.7.3

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -8,7 +8,7 @@ import (
 
 var Root = &cobra.Command{
 	Use:     "synthient",
-	Version: "v1.7.2",
+	Version: "v1.7.3",
 	Short:   "Official CLI tool for Synthient",
 	Long:    "Synthient's official CLI for account inspection, IP and domain lookups, gRPC schemas, real-time feed streams, and Parquet snapshot downloads.",
 	Run: func(cmd *cobra.Command, args []string) {
